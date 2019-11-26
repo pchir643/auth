61a1bd80e204d5a3a34ae7e80a24eee6fa708ed6
philippe CHIRIE <p.chirie@laposte.net>
2019-11-26 08:51:45 +0100

    Validation

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+   def index
+     @users = User.all
+  end
+end
